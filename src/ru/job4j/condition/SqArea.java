344d2a9d6b0795b9c4694912eacb7c03e48a3b89
AvetisM <avetis.mkhitaryants@gmail.com>
2021-03-24 13:20:59 +0300

    9.2. Checkstyle [#502839]

--- src/ru/job4j/condition/SqArea.java
+++ src/ru/job4j/condition/SqArea.java
@@ -4,9 +4,9 @@ public class SqArea {
 
     public static double square(double p, double k) {
 
-        double h = p / (2 * (k + 1)) ;
+        double h = p / (2 * (k + 1));
 
-        return Math.pow(h,2) * k;
+        return Math.pow(h, 2) * k;
     }
 
     public static void main(String[] args) {
