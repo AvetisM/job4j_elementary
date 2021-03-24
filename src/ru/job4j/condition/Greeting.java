55f789b8268bba008e6d48ea340874a58f44868d
AvetisM <avetis.mkhitaryants@gmail.com>
2021-03-24 12:40:21 +0300

    9.1. String [#502833]

---
+++ src/ru/job4j/condition/Greeting.java
@@ -0,0 +1,19 @@
+package ru.job4j.condition;
+
+import java.sql.SQLOutput;
+
+public class Greeting {
+
+    public static void main(String[] args) {
+
+        String idea = "I like Java!";
+        System.out.println(idea);
+
+        String separate = " ";
+        idea = idea + separate +"But I am a newbie.";
+        int year = 2021;
+        idea = idea + separate + year;
+        System.out.println(idea);
+
+    }
+}
