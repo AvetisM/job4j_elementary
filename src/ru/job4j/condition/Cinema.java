c726ae7f827a3570a8a5b061fc1594fbee09a456
AvetisM <avetis.mkhitaryants@gmail.com>
2021-03-25 20:31:16 +0300

    4.3.1. Оператор && [#502776]

--- src/ru/job4j/condition/Cinema.java
+++ src/ru/job4j/condition/Cinema.java
@@ -11,9 +11,21 @@ public class Cinema {
         }
     }
 
+    public static void permission(boolean allowByParent, boolean hasMoney) {
+        if (allowByParent && hasMoney) {
+            System.out.println("I can go to the cinema.");
+        } else {
+            System.out.println("I can't.");
+        }
+    }
+
     public static void main(String[] args) {
-        Cinema.access(17);
-        Cinema.access(18);
-        Cinema.access(19);
+
+        access(18);
+
+        permission(true, true);
+        permission(true, false);
+        permission(false, true);
+        permission(false, false);
     }
 }
